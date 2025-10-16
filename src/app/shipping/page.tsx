43b9408e73b0e73eaf618021630142ca9d830449
Your Name <ansokwin@gmail.com>
2025-10-16 22:13:50 +0700

    sdfsdf

--- src/app/shipping/page.tsx
+++ src/app/shipping/page.tsx
@@ -136,7 +136,7 @@ export default function ShippingPage() {
             </AnimatedSection>
           </div>
 
-          <div className="max-w-7xl mx-auto">
+          <div className="max-w-4xl mx-auto">
             <AnimatedSection className="text-center mb-0">
               <div className="w-16 h-1 bg-orange-400 mx-auto mb-4"></div>
               <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4">Our Delivery Partners</h2>
